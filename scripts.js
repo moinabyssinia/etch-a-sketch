9de8bf67f4494d5019e806b48f5e5f3882044761
moinabyssinia <michaelg.tadesse@gmail.com>
2020-07-30 07:59:35 -0400

    created 16 grids with JS

--- scripts.js
+++ scripts.js
@@ -4,15 +4,23 @@ const numOfDivs = 16;
 //add 16 square divs in the first line
 let cols = 0;
 let rows = 0;
-while (cols < numOfDivs){
-    //make the rows
-    while (rows < numOfDivs){
-        let newDiv = document.createElement('div');
-        newDiv.setAttribute('class', 'grids-rows');
-        newDiv.classList.add('grids-rows');
-        oldDiv.appendChild(newDiv);
-        rows++;
-    }
+while (rows < numOfDivs){
+    //create a container for each row
+    let rowContainer = document.createElement('div');
+    rowContainer.setAttribute('class', 'rowContainer');
+    rowContainer.classList.add('rowContainer');
+    oldDiv.appendChild(rowContainer);
 
+    //make the rows
+    while (cols < numOfDivs){
+        //insert the grids 
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'grids-cols');
+        newDiv.classList.add('grids-cols');
+        rowContainer.appendChild(newDiv);
+        cols++;
+    }
+    cols = 0;
+    rows++;
 }
 
