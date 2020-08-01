c76cc8caa333760b8a11d8fa703c36b32a2a73fb
moinabyssinia <michaelg.tadesse@gmail.com>
2020-08-01 08:24:46 -0400

    added a prompt button

--- scripts.js
+++ scripts.js
@@ -1,44 +1,56 @@
-const oldDiv = document.querySelector('.container');
-const numOfDivs = 16;
-
-//add 16 square divs in the first line
+let numOfDivs;
 let cols = 0;
 let rows = 0;
-while (rows < numOfDivs){
-    //create a container for each row
-    let rowContainer = document.createElement('div');
-    rowContainer.setAttribute('class', 'rowContainer');
-    rowContainer.classList.add('rowContainer');
-    oldDiv.appendChild(rowContainer);
+const oldDiv = document.querySelector('.container');
+const startButton = document.querySelector('.start-button');
 
-    //make the rows
-    while (cols < numOfDivs){
-        //insert the grids 
-        let newDiv = document.createElement('div');
-        newDiv.setAttribute('class', 'grids-cols');
-        newDiv.classList.add('grids-cols');
-        rowContainer.appendChild(newDiv);
-        cols++;
+
+//add functionality to the button
+startButton.addEventListener('click', function(){
+    // oldDiv.style.display = 'none';
+    numOfDivs = Number(prompt("How many squares do you want per side?"));
+    buildGrids();
+
+    //add hovering event listener
+    let gridColumns = document.querySelectorAll('.grids-cols');
+
+    gridColumns.forEach(function(element){
+        element.addEventListener('mouseenter', function(){
+            element.style.backgroundColor = changeColor();
+        });
+        // tried adding a css class on mouseout but didnt work
+        element.addEventListener('mouseout', function(){
+            setTimeout(function(){
+                element.style.backgroundColor = 'blue';
+            }, 500);
+        });
+    })
+})
+
+
+
+const buildGrids = function(){
+    while (rows < numOfDivs){
+        //create a container for each row
+        let rowContainer = document.createElement('div');
+        rowContainer.setAttribute('class', 'rowContainer');
+        rowContainer.classList.add('rowContainer');
+        oldDiv.appendChild(rowContainer);
+    
+        //make the rows
+        while (cols < numOfDivs){
+            //insert the grids 
+            let newDiv = document.createElement('div');
+            newDiv.setAttribute('class', 'grids-cols');
+            newDiv.classList.add('grids-cols');
+            rowContainer.appendChild(newDiv);
+            cols++;
+        }
+        cols = 0;
+        rows++;
     }
-    cols = 0;
-    rows++;
 }
 
-
-//add hovering event listener
-//use forEach to select each grid div
-let gridColumns = document.querySelectorAll('.grids-cols');
-gridColumns.forEach(function(element){
-    element.addEventListener('mouseenter', function(){
-        element.style.backgroundColor = changeColor();
-    });
-    // tried adding a css class on mouseout but didnt work
-    element.addEventListener('mouseout', function(){
-        setTimeout(function(){
-            element.style.backgroundColor = 'blue';
-        }, 500);
-    });
-})
 
 // change the background to random color
 const changeColor = function(){
@@ -47,3 +59,4 @@ const changeColor = function(){
 const generateNumbers = function(){
     return Math.ceil(Math.random()*255);
 }
+
